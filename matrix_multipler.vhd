b61f3719e0aa6cf82251afad5bb319723eb9faf9
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2019-05-31 20:14:54 +0430

    fix

--- matrix_multipler.vhd
+++ matrix_multipler.vhd
@@ -18,13 +18,15 @@ entity matrix_multipler is
            rst : in std_logic;
            enable_a : in std_logic;
            enable_b : in std_logic;
+           enable_r : in std_logic;
            input_a : in std_logic_vector(31 downto 0);
            input_b : in std_logic_vector(31 downto 0);
-           ready : in std_logic;
-           output : out std_logic_vector(31 downto 0));
+           ready : out std_logic;
+           output : out std_logic_vector(31 downto 0)
+         );
 end matrix_multipler;
 architecture Behavioral of matrix_multipler is
-  type first_mem is array (0 to input_a_rows-1,0 to input_a_cols-1) of std_logic_vector(31 downto 0);
+  type first_mem  is array (0 to input_a_rows-1,0 to input_a_cols-1) of std_logic_vector(31 downto 0);
   type second_mem is array (0 to input_b_rows-1,0 to input_b_cols-1) of std_logic_vector(31 downto 0);
   type result_mem is array (0 to input_a_rows-1,0 to input_b_cols-1) of std_logic_vector(31 downto 0);
   
@@ -42,6 +44,7 @@ begin
     if rising_edge(clk) then
       if rst='1' then
         a_ready <= '0';
+        ready <= '0';  
       elsif enable_a='1' then
         if(a_ready ='0' ) then
           firstmem(i,j) <= input_a;
@@ -65,6 +68,7 @@ begin
     if rising_edge(clk) then
       if rst='1' then
         b_ready <= '0';
+        ready   <= '0';  
       elsif enable_b='1' then
         if(b_ready ='0' ) then
           secondmem(i,j) <= input_b;
@@ -96,8 +100,30 @@ begin
               end loop;
                resultmem(i,j) <= single_float_to_slv(sum);
             end loop;
-      end loop;  
+      end loop;
+      ready <= '1';  
     end if;
   end process;
+
+process(clk)
+  variable i : integer := 0;
+  variable j : integer := 0;
+  begin
+    if rising_edge(clk) then
+      if rst='1' then
+        ready <= '0';  
+      elsif enable_r='1' then
+          output <= resultmem(i,j);
+          i := i+1;
+      end if;
+      if(i = input_b_rows) then 
+        i := 0;
+        j := j+1;
+      end if;
+      if(j = input_b_cols) then 
+        j:=0;
+      end if;
+    end if;
+  end process;  
 
 end Behavioral;
