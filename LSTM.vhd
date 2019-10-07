19a36ad63ec25b7985221979d65585a7822feda5
Mohammad Mahdi Rahimi <mohammadmahdi76@Gmail.com>
2019-10-07 18:05:52 +0330

    final fix

--- LSTM.vhd
+++ LSTM.vhd
@@ -6,11 +6,11 @@ use IEEE.numeric_std.all;
 library work;
 use work.neurals_utils.all;
 
-
 entity LSTM is
   port (
-	clk   : in  std_logic;
-	x     : in  matrix_1_4;
+	  clk   : in  std_logic;
+    rst   : in  std_logic;
+	  x     : in  matrix_1_4;
     h_old : in  matrix_1_8;
     c_old : in  matrix_1_8;
     c_new : out matrix_1_8;
@@ -55,23 +55,18 @@ architecture arch of LSTM is
     );
 	end component;
 
-	component matrix_multipler is
-    Generic (
-      input_a_rows : integer := 3;
-      input_a_cols : integer := 3;
-      input_b_rows : integer := 3;
-      input_b_cols : integer := 3
-    );
+	component sop is
     Port (
-           clk : in std_logic;
-           rst : in std_logic;
-           enable_a : in std_logic;
-           enable_b : in std_logic;
-           input_a : in std_logic_vector(31 downto 0);
-           input_b : in std_logic_vector(31 downto 0);
-           ready : in std_logic;
-           output : out std_logic_vector(31 downto 0));
-	end component;
+          clk : in std_logic;
+          rst : in std_logic;
+          X : in  matrix_1_4;
+          W : in  matrix_4_8;
+          H : in  matrix_1_8;
+          U : in  matrix_8_8;
+          B : in  matrix_1_8;
+          O : out matrix_1_8
+         );
+  end component;
 
 
 signal W_o : matrix_4_8;
@@ -98,24 +93,36 @@ signal tanh_ct : matrix_1_8;
 signal candidate_value : matrix_1_8;
 signal final_c : matrix_1_8;
 signal forget : matrix_1_8;
+signal input_1 : matrix_1_8;
+signal output_1 : matrix_1_8;
 signal mul_c_ft : matrix_1_8;
-signal mul_It_Ct : matrix_1_8;
+signal mul_It_Ct_1 : matrix_1_8;
+
+signal WI, WF, WC, WO : matrix_4_8;
+signal UI, UF, UC, UO : matrix_8_8;
+signal BI, BF, BC, BO : matrix_1_8;
 
 begin
-
 tanh_c_2_h : tanh_matrix_1_8 port map (clk, '1', final_c, tanh_ct);
 tanh_c_2_c : tanh_matrix_1_8 port map (clk, '1', candidate_value, C_t);
 
 sigmoid_forget_ft : sigmoid_matrix_1_8 port map (clk, '1', forget, F_t);
-sigmoid_input_it  : sigmoid_matrix_1_8 port map (clk, '1', forget, I_t);
-sigmoid_output_ot : sigmoid_matrix_1_8 port map (clk, '1', forget, O_t);
+sigmoid_input_it  : sigmoid_matrix_1_8 port map (clk, '1', input_1, I_t);
+sigmoid_output_ot : sigmoid_matrix_1_8 port map (clk, '1', output_1, O_t);
 
 mul_c_old_ft : mul_mat_1_8 port map (c_old, F_t, mul_c_ft);
-mul_it_ct    : mul_mat_1_8 port map (I_t, C_t, mul_It_Ct);
+mul_it_ct    : mul_mat_1_8 port map (I_t, C_t, mul_It_Ct_1);
 mul_tanhc_ot : mul_mat_1_8 port map (O_t, tanh_ct, h_new);
 
-add_m : add_mat_1_8 port map (mul_c_ft, mul_It_Ct, c_new);
+add_m : add_mat_1_8 port map (mul_c_ft, mul_It_Ct_1, c_new);
 
+WI <= init_WI; WF <= init_WF; WC <= init_WC; WO <= init_WO;
+UI <= init_UI; UF <= init_UF; UC <= init_UC; UO <= init_UO;
+BI <= init_BI; BF <= init_BF; BC <= init_BC; BO <= init_BO;
 
+input_sop           : sop port map (clk, rst, x, WI, h_old, UI, BI, input_1); -- weight should be filled here
+forget_sop          : sop port map (clk, rst, x, WF, h_old, UF, BF, forget); -- weight should be filled here
+candidate_value_sop : sop port map (clk, rst, x, WC, h_old, UC, BC, candidate_value); -- weight should be filled here
+output_sop          : sop port map (clk, rst, x, WO, h_old, UO, BO, output_1); -- weight should be filled here
 
 end architecture ; -- arch
--- mat_mul_148.vhd
+++ mat_mul_148.vhd
@@ -47,27 +47,14 @@ signal input_b  : std_logic_vector(31 downto 0);
 signal ready    : std_logic;
 signal output   : std_logic_vector(31 downto 0);
 
-type state is (S0, S1, S2, S3, S35, S4, SW, S5, SH);
+type state is (S0, S1, S2, S3);
 signal current_state : state;
 signal next_state : state;
 
 begin
-
 matmul_m: matrix_multipler 
             generic map (1,4,4,8) 
             port map (clk, rst, enable_a, enable_b, enable_r, input_a, input_b, ready, output);
-
-  MATMUL_PRC : process( clk )
-  variable a_i : integer := 0;
-  variable a_j : integer := 0;
-  variable b_i : integer := 0;
-  variable b_j : integer := 0;
-  begin
-    if clk'event and clk = '1' then 
-
-    end if;
-  end process ; -- MATMUL_PRC
-
   -- next to current
   SM_PRC: process (clk, rst)
   begin
@@ -79,50 +66,44 @@ matmul_m: matrix_multipler
   end process;
 
   -- next based on state
-  process (current_state)
+  process (current_state,clk)
   variable a_i : integer := 0;
-  variable a_j : integer := 0;
   variable b_i : integer := 0;
   variable b_j : integer := 0;
   variable r_i : integer := 0;
-  variable r_j : integer := 0;
   begin
     case current_state is
       when S0 =>
         enable_a <= '0';
         enable_b <= '0';
         enable_r <= '0';
-        o_ready <= '0';
-        a_i = -1; a_j = 0;
-        b_i = -1; b_j = 0;
-        r_i = -1; r_j = 0;
+        o_ready  <= '0';
+        a_i := -1;
+        b_i := -1; b_j := 0;
+        r_i := -1;
         next_state <= S1;
       when S1 => 
         enable_a <= '1';
         enable_b <= '1';
-        a_i += 1;
-        b_i += 1;
-        if a_i = 4 then a_i = 0; a_j += 1; end if;
-        if a_j < 1 then
-          input_a <= input_mat_A(a_i);
-        end if;
-        if b_i = 4 then a_i = 0; a_j += 1; end if;
-        if b_j < 8 then
-          input_b <= input_mat_B(b_i, b_j);
-        end if;
-        if b_j >= 8 and a_j >= 1 then 
+        a_i := a_i + 1;
+        b_i := b_i + 1;
+        
+        if a_i < 4 then input_a <= input_mat_A(a_i); end if;
+        
+        if b_i = 4 then b_i := 0; b_j := b_j + 1; end if;
+        if b_j < 8 then input_b <= input_mat_B(b_i, b_j); end if;
+        
+        if b_j >= 8 and a_i >= 4 then 
           next_state <= S2;
         end if;
       when S2 =>
         if ready = '1' then
           enable_r <= '1';
-          r_i += 1;
-          if r_i = 4 then r_i = 0; r_j += 1; end if;
-          if r_j < 1 then
-            output_mat(r_i) <= output;
-          end if;
+          r_i := r_i + 1;
+          if r_i < 4 then output_mat(r_i) <= output; end if;
         end if;
-        if r_j >= 1 then
+
+        if r_i >= 4 then
           next_state <= S3;
         end if;
       when S3 =>
